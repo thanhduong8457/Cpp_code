b6b3154317b2488b743b3f46d558c33bf7334ca8
Le Thanh Duong <duong.le-thanh@internal.banvien.com>
2024-01-05 18:01:45 +0700

    add code

---
+++ systemc/pwmPulseGen/dummy.h
@@ -0,0 +1,22 @@
+#include "systemc.h"
+
+class dummy: public sc_module {
+    sc_out<bool> dummy_enable;
+    sc_out<bool> dummy_preset;
+    sc_out<bool> dummy_wr;
+
+    sc_out< sc_dt::uint64 > dummy_N;
+    sc_out< sc_dt::uint64 > dummy_Nx;
+
+    sc_in<bool> dummy_pulse_x_in;
+    sc_in<bool> dummy_dir_x_in;
+    sc_in<bool> dummy_flag_in;
+    sc_in<bool> dummy_clk1_in;
+
+    //pointting to tracefile in contructor
+    sc_trace_file *pTraceFile;
+
+    dummy(void);
+    void simulation();
+
+};
---
+++ systemc/pwmPulseGen/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iterator>
+#include <map>
+
+#include "pwmPulseGen.h"
+
+using namespace std;
+  
+int main()
+{
+
+
+   return 0;
+}
