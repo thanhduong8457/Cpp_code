63909ad4512df8442c808e24a9b138b67d5efdd5
thanhduong8457 <thanhduong2625@gmail.com>
2024-09-08 23:32:16 +0700

    add crc code

--- demo_cpp/main copy.cpp
+++ demo_cpp/main copy.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
 #include <vector>
 
-const uint32_t FRAME_CRC_POLY = 0x5d6dcb;  // Frame CRC polynomial
-const uint32_t HEADER_CRC_POLY = 0x385;  // Header CRC polynomial
+const uint32_t FRAME_CRC_POLY = 0x1F23B;  // Frame CRC polynomial
+const uint32_t HEADER_CRC_POLY = 0x0F4D;  // Header CRC polynomial
 
 /// @brief 
 /// @param data 
 /// @param sizeCrc 
 /// @param poly 
 /// @return 
-uint32_t calculateCRC(uint8_t* data, uint32_t lengthInByte, uint32_t sizeCrc, uint32_t init, uint32_t poly) {
-    uint32_t maskMSB = 1 << (sizeCrc - 1);
-    uint32_t mask = (1 << sizeCrc) - 1;
-    uint32_t crc = init;
-    std::cout << "crc = 0x" << std::hex << crc << std::endl;
+uint32_t calculateCRC(uint8_t* data, uint32_t lengthInByte, unsigned int sizeCrc, uint32_t poly) {
+    uint32_t maskMSB = 1 << sizeCrc;
+    uint32_t mask = maskMSB - 1;
+    uint32_t crc = mask;
+    std::cout << "maskMSB 0x" << std::hex << maskMSB << std::endl;
+    std::cout << "mask 0x" << std::hex << mask << std::endl;
 
     for (uint8_t index = 0x0; index < lengthInByte; ++index) {
+        std::cout << "data byte 0x" << std::hex << (static_cast<uint32_t>(data[index])) << std::endl;
+        std::cout << "data byte after shift 0x" << std::hex << (static_cast<uint32_t>(data[index]) << (sizeCrc - 8)) << std::endl;
         crc ^= ((static_cast<uint32_t>(data[index]) << (sizeCrc - 8)));
         for (int i = 0; i < 8; ++i) {
-            if (0x0 != (crc & maskMSB) >> 1) {
+            if (crc & (maskMSB >> 1)) {
                 crc = (crc << 1) ^ poly;
             } else {
                 crc <<= 1;
             }
         }
-        std::cout << "crc = 0x" << std::hex << crc << std::endl;
     }
     return crc & mask;
 }
@@ -37,9 +39,9 @@ uint32_t calculateCRC(uint8_t* data, uint32_t lengthInByte, uint32_t sizeCrc, ui
 bool verifyCRC(uint8_t* data, uint32_t dataLength, uint32_t transmittedCRC, bool isFrameCRC = true) {
     uint32_t calculatedCRC;
     if (isFrameCRC) {
-        calculatedCRC = calculateCRC(data, dataLength, 24, 0xabcdef, FRAME_CRC_POLY);
+        calculatedCRC = calculateCRC(data, dataLength, 24, FRAME_CRC_POLY);
     } else {
-        calculatedCRC = calculateCRC(data, dataLength, 11, 0x1a, HEADER_CRC_POLY);
+        calculatedCRC = calculateCRC(data, dataLength, 11, HEADER_CRC_POLY);
     }
 
     return (calculatedCRC == transmittedCRC);
@@ -56,14 +58,14 @@ int main() {
     headerData[0] = 0x12;
     headerData[1] = 0x34;
 
-    payloadData[0] = 0xfB;
-    payloadData[1] = 0xfD;
-    payloadData[2] = 0xfF;
+    payloadData[0] = 0xAB;
+    payloadData[1] = 0xCD;
+    payloadData[2] = 0xEF;
 
-    uint32_t frameCRC = calculateCRC(payloadData, 3, 24, 0xabcdef, FRAME_CRC_POLY);
+    uint32_t frameCRC = calculateCRC(payloadData, 3, 24, FRAME_CRC_POLY);
     std::cout << "Calculated Frame CRC (24-bit): 0x" << std::hex << frameCRC << std::endl;
 
-    uint16_t headerCRC = calculateCRC(headerData, 2, 11, 0x1a, HEADER_CRC_POLY);
+    uint16_t headerCRC = calculateCRC(headerData, 2, 11, HEADER_CRC_POLY);
     std::cout << "Calculated Header CRC (11-bit): 0x" << std::hex << headerCRC << std::endl;
 
     // uint32_t transmittedFrameCRC = frameCRC;
